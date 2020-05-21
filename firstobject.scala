3e4d763ee7950dfec1148623647465f698c4624b
TRC98 <chamodrajitha98@gmail.com>
2020-05-21 23:25:07 +0530

    Answers of assignment 1 in scala

---
+++ firstobject.scala
@@ -0,0 +1,35 @@
+
+
+object Assingment1 {
+ 
+  def main(args: Array[String]){
+    
+ println("Answer to question 1")
+ println(function1)
+ println("")
+ println("Answer to question 2")
+ println(function2)
+ println("")
+ println("Answer to question 3")
+ println(function3)
+ 
+  } 
+  def function1{
+  println("35 celsius convert to fahrenheit")
+  var result =35*1.8 +32
+  println (result)
+  }
+
+  
+  def function2{
+    println("The volume of a sphere with radius 5")
+  var volume = (4.0*22*5*5*5)/(3*7)
+  println(volume)
+  }
+  
+  def function3{
+    println("The total cost is")
+     var cost=(50*3+0.75*10)+24.95*60*60/100
+     println(cost)
+  }
+}
