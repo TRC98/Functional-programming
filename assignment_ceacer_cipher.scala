0dfd7ea8ebd129c5dc7fdea55f6124beedc46a92
TRC98 <65697869+TRC98@users.noreply.github.com>
2020-07-19 22:46:27 +0530

    Update assignment_ceacer_cipher.scala

--- assignment_ceacer_cipher.scala
+++ assignment_ceacer_cipher.scala
@@ -1,80 +1,86 @@
 
-
 object assignment_ceacer {
    def main(args:Array[String]){
     var arr1=Array('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z')
     
-    println("1.Encrypt message")
-    println("2.Decrypt message")
-    var n=scala.io.StdIn.readLine()
-    if(n=="1"){
-    println("Enter your message in lower case:")
-    var input=scala.io.StdIn.readLine()
-    
-    var count: Int=input.toCharArray.length
-    var x:Int=count
-    var arr2=new Array[Int](count)
-    var j:Int=0
-    for( i <- input){
-     
-      if(i.isLetter){
-       
-        var fl:Int=0
-        
-        while(i != arr1(fl)){
-          fl=fl+1
-          
-      }
-        if(fl>2){
-          var ptr=(fl-3)
-          print(arr1(ptr))
-        }
-        else{
-          var ptr=(26+(fl-3))
-          print(arr1(ptr))
-        }
-    }
-      else{
-        print(i)
-      }
-    
-         j=j+1
-    }
-   }
-    else if(n=="2"){
-       println("Enter your message in lower case:")
-    var input=scala.io.StdIn.readLine()
+    while(true){
+        println("")
+        println("1.Encrypt message")
+        println("2.Decrypt message")
+        var n=scala.io.StdIn.readLine()
+        if(n=="1"){
+        println("Enter your message to encrypt in lower case:")
+        var input=scala.io.StdIn.readLine()
+        println("Enter the key number less than 26")
+        var key= scala.io.StdIn.readInt()
         var count: Int=input.toCharArray.length
-    var x:Int=count
-    var arr2=new Array[Int](count)
-    var j:Int=0
-    for( i <- input){
-     
-      if(i.isLetter){
-       
-        var fl:Int=0
-        
-        while(i != arr1(fl)){
-          fl=fl+1
-          
-      }
-        if(fl<23){
-          var ptr=fl+3
-        print(arr1(ptr))
-        
-    }
-        else{
-           var ptr=((fl+3)%25)-1
-        print(arr1(ptr))
-        }
-        }
+        var x:Int=count
+        var arr2=new Array[Int](count)
+        var j:Int=0
+        for( i <- input){
          
-    
-      else{
-        print(i)
-      }
-      j=j+1
-   }
+          if(i.isLetter){
+           
+            var fl:Int=0
+            
+            while(i != arr1(fl)){
+              fl=fl+1
+              
+          }
+            if(fl>key-1){
+              var ptr=(fl-key)
+              print(arr1(ptr))
+            }
+            else{
+              var ptr=(26+(fl-key))
+              print(arr1(ptr))
+            }
+        }
+          else{
+            print(i)
+          }
+        
+             j=j+1
+        }
+       }
+        else if(n=="2"){
+           println("Enter your message to decrypt in lower case:")
+        var input=scala.io.StdIn.readLine()
+        println("Enter the key number less than 26")
+        var key= scala.io.StdIn.readInt()
+            var count: Int=input.toCharArray.length
+        var x:Int=count
+        var arr2=new Array[Int](count)
+        var j:Int=0
+        for( i <- input){
+         
+          if(i.isLetter){
+           
+            var fl:Int=0
+            
+            while(i != arr1(fl)){
+              fl=fl+1
+              
+          }
+            if(fl<26-key){
+              var ptr=fl+key
+            print(arr1(ptr))
+            
+        }
+            else{
+               var ptr=((fl+key)%25)-1
+            print(arr1(ptr))
+            }
+            }
+             
+        
+          else{
+            print(i)
+          }
+          j=j+1
+       }
+        }
     }
+   
 }
 }
